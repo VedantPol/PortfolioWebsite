f2c7dbfb6cb358176b2770d84c7f276e0cb57d1c
VedantPol <polvedant7@gmail.com>
2022-08-10 10:31:18 +0530

    added coursels

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import AnimatedText from 'react-animated-text-content';
-import { Row ,Col} from 'react-bootstrap';
+import { Row ,Col, Carousel} from 'react-bootstrap';
+import 'holderjs';
+
 
 function App() {
   return (
+    
     <div className="App">
+
         <div className='Navbar'>
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
@@ -61,11 +65,52 @@ function App() {
   threshold={0.1}
   rootMargin="30%"
 >
-  Content to animate
+Designer, Frontend Developer & Mentor
+I design and code beautifully simple things, and I love what I do.
 </AnimatedText>
 
 </Col>
       </Row>
+     
+      <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=First slide&bg=373940"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=Second slide&bg=282c34"
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=Third slide&bg=20232a"
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
       </div >
     </div>
   );
